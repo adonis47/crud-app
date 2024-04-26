a083d03700e2031d7c05d589027b9bb6fef6cf55
Dilane Waffo <dianewaffo81@gmail.com>
2024-04-26 20:51:45 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Footer from "./components/Footer"
 function App() {
   return (
     <>
       {" "}
-      <h1 className="text-3xl text-red-500 font-bold underline">
-        Bravo !!!
+      <h1 className=" font-bold underline">
+        <Footer/>
       </h1>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <div className='bg-blue-500 fixed bottom-0 text-xl w-full p-6 text-center text-white'>
+        <span className='m-14'>Completed Todos</span>
+        <span className='m-16'>Total Todos</span>
+    </div>
+  )
+}
+
+export default Footer
